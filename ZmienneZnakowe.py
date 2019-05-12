616689df30c521507da629946348f61966ae54a3
emamaj <41389232+emamaj@users.noreply.github.com>
2019-05-12 18:31:37 +0200

    Add files via upload

---
+++ ZmienneZnakowe.py
@@ -0,0 +1,30 @@
+import string
+
+dlugaZmienna = "Tworzymy całe zdanie w jednej zmiennej\, ktore bedzie zapisane w wielu wierszach.\ Wszystko dzieki zastosowaniu znaku konca linii, czyli '\'."
+
+print(dlugaZmienna)
+
+print("""
+kolejne
+dlugie
+zdanie
+""")
+
+print('''
+Podobnie
+    jak wyżej''')
+
+slowo1 = "Abc"
+print (slowo1 * 3)
+
+slowo2 = "zazizuzizi"
+print(slowo2[4])
+print(slowo2[2:7])
+print(slowo2[3:])
+print(slowo2[:3])
+print(slowo2[-3:])
+print(slowo2[:-3])
+
+slowo3 = "braZYLIA"
+print(slowo3.capitalize())
+print(slowo3)
